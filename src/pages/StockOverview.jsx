ebea6e56c76a87115b7e6eb7c316e2c45ddf210f
chivicoko <vicharmonic@gmail.com>
2023-01-29 02:43:02 +0000

    image adjustment ...

--- src/pages/StockOverview.jsx
+++ src/pages/StockOverview.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import AutoComplete from '../components/AutoComplete';
 import StockList from '../components/StockList';
 // import tradeImage from '../trade.jpg'
-// import tradeImage from '../trade-removebg-preview.png'
+import tradeImage from '../trade-removebg-preview.png'
 
 const StockOverview = () => {
   return (
     <div className='mt-5'>
-      {/* <div className='text-center'>
+      <div className='text-center'>
         <h1>TradeMatic</h1>
         <img src={tradeImage} alt="Trade" className='mb-3 trade-image' style={{width: '150px'}} />
-      </div> */}
+      </div>
         <AutoComplete />
         <StockList />
     </div>
